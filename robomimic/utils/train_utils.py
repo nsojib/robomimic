7860c6aeffe86f166c05ac7650f80746b9647bfe
Noushad Sojib <noushad.sust@gmail.com>
2025-08-29 16:40:29 -0400

    n

--- robomimic/utils/train_utils.py
+++ robomimic/utils/train_utils.py
@@ -57,7 +57,8 @@ def get_exp_dir(config, auto_remove_exp_dir=False):
     base_output_dir = os.path.join(base_output_dir, config.experiment.name)
     if os.path.exists(base_output_dir):
         if not auto_remove_exp_dir:
-            ans = input("WARNING: model directory ({}) already exists! \noverwrite? (y/n)\n".format(base_output_dir))
+            # ans = input("WARNING: model directory ({}) already exists! \noverwrite? (y/n)\n".format(base_output_dir))
+            ans='n'  # for auto run
         else:
             ans = "y"
         if ans == "y":
